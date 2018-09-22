f2860647caae44583bbfcda8cfffd0b043cff544
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-09-21 22:36:15 -0700

    Get libsnappy FFI working on macOS

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
     println!("cargo:rustc-link-search=./lib");
+    println!("cargo:rustc-link-lib=c++");
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,83 @@
 extern crate libc;
-use libc::size_t;
+use libc::{c_int, size_t};
 
 #[link(name = "snappy")]
-extern {
-  fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+extern "C" {
+    fn snappy_max_compressed_length(source_length: size_t) -> size_t;
+    fn snappy_uncompressed_length(
+        compressed: *const u8,
+        compressed_length: size_t,
+        result: *mut size_t,
+    ) -> c_int;
+
+    fn snappy_compress(
+        input: *const u8,
+        input_length: size_t,
+        compressed: *mut u8,
+        compressed_length: *mut size_t,
+    ) -> c_int;
+    fn snappy_uncompress(
+        compressed: *const u8,
+        compressed_length: size_t,
+        output: *mut u8,
+        output_length: *mut size_t,
+    ) -> c_int;
+}
+
+/// Safe wrapper around snappy_compress
+fn safe_compress(input: &[u8]) -> Vec<u8> {
+    let input_len = input.len() as size_t;
+    let mut output_len = unsafe { snappy_max_compressed_length(input_len) };
+    let mut output = Vec::with_capacity(output_len as usize);
+    unsafe {
+        let status = snappy_compress(
+            input.as_ptr(),
+            input_len,
+            output.as_mut_ptr(),
+            &mut output_len,
+        );
+        assert_eq!(status, 0);
+        assert!(output_len <= output.capacity());
+        output.set_len(output_len);
+    };
+    output
+}
+
+/// Safe wrapper around snappy_uncompress
+fn safe_uncompress(input: &[u8]) -> Vec<u8> {
+    let input_len = input.len() as size_t;
+    let mut output_len = 0;
+    unsafe {
+        let status =
+            snappy_uncompressed_length(input.as_ptr(), input.len() as size_t, &mut output_len);
+        assert_eq!(status, 0);
+    };
+    let mut output = Vec::with_capacity(output_len as usize);
+    unsafe {
+        let status = snappy_uncompress(
+            input.as_ptr(),
+            input_len,
+            output.as_mut_ptr(),
+            &mut output_len,
+        );
+        assert_eq!(status, 0);
+        assert!(output_len <= output.capacity());
+        output.set_len(output_len);
+    };
+    output
 }
 
 fn main() {
-  let x = unsafe { snappy_max_compressed_length(100) };
-  println!("max compressed length of a 100 byte buffer: {}", x);
+    let text = String::from("Hello, World!");
+    let compressed = safe_compress(text.as_bytes());
+    let uncompressed = safe_uncompress(&compressed);
+    println!("text: {}", text);
+    println!("raw: {:?}", text.as_bytes());
+    println!("compressed: {:?}", compressed);
+    println!("uncompressed: {:?}", uncompressed);
+    if uncompressed == text.as_bytes() {
+        println!("they match!");
+    } else {
+        println!("[ERROR] uncompressed text does not match original");
+    }
 }
